7bf1e57fbb972abe26cb6ae38de3f8907fd8e552
Elhamu <38259572+Elhamu@users.noreply.github.com>
2019-11-21 14:15:55 +0200

    Create, update and search

--- js/copy.js
+++ js/copy.js
@@ -1,6 +1,10 @@
+var currentIndex = 0;
 var siteName = document.getElementById('sitename');
 var urlName = document.getElementById('siteurl');
 var myBtn = document.getElementById('mybtn');
+var searchInp = document.getElementById('searchInp');
+var rowData = document.getElementById('rowData');
+
 var siteContainer;
  if(localStorage.getItem('siteContainer')== null){
 	siteContainer = [];
@@ -10,11 +14,32 @@ var siteContainer;
  	siteContainer = JSON.parse(localStorage.getItem('siteContainer'));
  	displaysite();
  }
+ searchInp.onkeyup=function(){
+searchData(searchInp.value)
+ }
+ function searchData(term){
+ 	
+ 		var searchCols = "";
+ 		for(var i=0;i<siteContainer.length;i++){
+ 			if(siteContainer[i].name.includes(term)){
+ 				searchCols+='<div class="col-md-4"><div class="sites"><h3>'+siteContainer[i].name+'</h3><a target="_blank"  class="btn btn-default" href='+siteContainer[i].url+'>Visit</a> </div></div>';
+rowData.innerHTML= searchCols;	
+}
+ 	}
+ }
 myBtn.onclick = function(){
+	if(myBtn.innerHTML == 'Add Site'){
 addsite();
 displaysite();
 clearform();
 }
+else{
+updateProduct();
+displaysite();
+}
+}
+
+
 function addsite(){
 	var site = {
 		name : siteName.value,
@@ -23,10 +48,17 @@ function addsite(){
 	siteContainer.push(site);
 	localStorage.setItem('siteContainer',JSON.stringify(siteContainer));
 }
+function updateProduct(){
+	 siteContainer[currentIndex].name = siteName.value ;
+	  siteContainer[currentIndex].url  = urlName.value;
+myBtn.innerHTML = "Add Site";
+
+	localStorage.setItem('siteContainer',JSON.stringify(siteContainer));
+	}
 function displaysite(){
 	var cols = "";
 	for (var i = 0; i < siteContainer.length; i++) {
-cols+='<div class="col-md-12"><div class="sites"><h3>'+siteContainer[i].name+'</h3><a  class="btn btn-default" href='+siteContainer[i].url+'>Visit</a><button class="btn btn-danger" onclick="delfun('+i+')">Delete</button> </div></div>';
+cols+='<div class="col-md-4"><div class="sites"><h3>'+siteContainer[i].name+'</h3><a  class="btn btn-default" href='+siteContainer[i].url+'>Visit</a><button class="btn btn-danger mr-2" onclick="delfun('+i+')">Delete</button><button class="btn btn-info" onclick="setForm('+i+')">Update</button> </div></div>';
 document.getElementById('myrow').innerHTML= cols;
 	}
 }
@@ -39,4 +71,12 @@ function clearform(){
 	siteContainer.splice(id,1);
 		localStorage.setItem("siteContainer",JSON.stringify(siteContainer));
 	displaysite();
+}
+
+function setForm(i){
+	 siteName.value = siteContainer[i].name;
+	 urlName.value = siteContainer[i].url;
+	 myBtn.innerHTML = "Update Site";
+	 currentIndex =i;
+	//alert(i)
 }
